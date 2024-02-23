2bc2550f69812308ec1c1be394daf07d9491abd8
Haider bokhari <67211098+SHaiderX@users.noreply.github.com>
2024-02-23 12:55:41 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@ module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
   pageExtensions: ['page.js', 'api.js'],
+    images: {
+    domains: ['example.com'], // List of domains where images are hosted
+    deviceSizes: [640, 768, 1024, 1280, 1600], // Array of sizes for responsive images
+    imageSizes: [16, 32, 48, 64, 96], // Sizes for image components
+    // Additional valid properties as needed
+  },
   webpack(config, { isServer }) {
     // Run custom scripts
     if (isServer) {
