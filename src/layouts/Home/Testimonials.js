6496083eee4ee786a81057ff2db93528e1437f72
SHaiderX <ShaiderHB@gmail.com>
2024-02-23 13:06:41 -0500

    another fix?

--- src/layouts/Home/Testimonials.js
+++ src/layouts/Home/Testimonials.js
@@ -69,8 +69,8 @@ export const Testimonials = ({ children, sectionRef }) => {
     const y = e.clientY - rect.top; // Y position within the element
     // Calculate rotation based on mouse position
     const limits = 15.0;
-    const rotateY = y * (limits * 2) - limits;
-    const rotateX = x * (limits * 2) - limits;
+    const rotateY = x * (limits * 2) - limits;
+    const rotateX = y * (limits * 2) - limits;
     // Apply 3D rotation
     card.style.transform = `perspective(1000px) rotateX(${-rotateX}deg) rotateY(${rotateY}deg)`;
   };
